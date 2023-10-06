40302f8b8f792d8e9909934514af3753515a7008
jeevanalexen <86559072+castlercodes@users.noreply.github.com>
2023-10-06 12:02:43 +0530

    some new changes

--- app/create/shop/page.js
+++ app/create/shop/page.js
@@ -1,9 +1,26 @@
+'use client';
 import React from 'react'
+import { useSearchParams } from 'next/navigation';
 
 function page() {
+
+  const searchParams = useSearchParams();
+  const userId = searchParams.get('userid');
+
   return (
     <div>
-      
+      <div className='bannercreateshop'>
+
+      </div>
+      <div className='flex flex-column'>
+        <input placeholder="Enter the Shop's name"/>
+        <input placeholder="Enter Address Line 1"/>
+        <input placeholder="Enter Address Line 2"/>
+        <input placeholder="Enter Pincode"/>
+        <input placeholder="Enter City"/>
+        <input placeholder="Enter State"/>
+        <input placeholder="Enter Country"/>
+      </div>
     </div>
   )
 }
