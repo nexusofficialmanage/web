285f2c67d5f6cdc1f45271c3a7dd36cf213e648e
jeevanalexen <86559072+castlercodes@users.noreply.github.com>
2023-10-06 12:49:38 +0530

    create shop page changges

--- app/create/shop/page.js
+++ app/create/shop/page.js
@@ -1,28 +1,67 @@
 'use client';
-import React from 'react'
+import React, { useState } from 'react';
+import './page.css';
 import { useSearchParams } from 'next/navigation';
+import { AiFillPlusCircle , AiOutlineMinusCircle} from 'react-icons/Ai';
 
-function page() {
-
+function Page() {
   const searchParams = useSearchParams();
   const userId = searchParams.get('userid');
 
+  // State to store phone numbers
+  const [phoneNumbers, setPhoneNumbers] = useState(['']);
+
+  // Function to add a new phone number input field
+  const addPhoneNumberInput = () => {
+    setPhoneNumbers([...phoneNumbers, '']);
+  };
+
+  // Function to remove a phone number input field
+  const removePhoneNumberInput = (index) => {
+    const newPhoneNumbers = [...phoneNumbers];
+    newPhoneNumbers.splice(index, 1);
+    setPhoneNumbers(newPhoneNumbers);
+  };
+
   return (
     <div>
-      <div className='bannercreateshop'>
+      <div className='bannercreateshop'></div>
+      <div className='shopinformation'>
+        <div className='shopinformationinput'>
+          <input placeholder="Enter the Shop's name" className='inpt' />
+          <input placeholder="Enter Address Line 1" className='inpt' />
+          <input placeholder="Enter Address Line 2" className='inpt' />
+          <input placeholder="Enter Pincode" className='inpt' />
+          <input placeholder="Enter City" className='inpt' />
+          <input placeholder="Enter State" className='inpt' />
+          <input placeholder="Enter Country" className='inpt' />
 
-      </div>
-      <div className='flex flex-column'>
-        <input placeholder="Enter the Shop's name"/>
-        <input placeholder="Enter Address Line 1"/>
-        <input placeholder="Enter Address Line 2"/>
-        <input placeholder="Enter Pincode"/>
-        <input placeholder="Enter City"/>
-        <input placeholder="Enter State"/>
-        <input placeholder="Enter Country"/>
+          {/* Phone Numbers */}
+          <div className='addoninput'>
+            {phoneNumbers.map((phoneNumber, index) => (
+              <div key={index}>
+                <input
+                  className='inpt'
+                  type="text"
+                  value={phoneNumber}
+                  placeholder={`Phone Number ${index + 1}`}
+                  onChange={(e) => {
+                    const newPhoneNumbers = [...phoneNumbers];
+                    newPhoneNumbers[index] = e.target.value;
+                    setPhoneNumbers(newPhoneNumbers);
+                  }}
+                />
+                {index > 0 && (
+                  <AiOutlineMinusCircle onClick={() => removePhoneNumberInput(index)}  className='btn'/>
+                )}
+              </div>
+            ))}
+            <AiFillPlusCircle onClick={addPhoneNumberInput} className='btn'/>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
