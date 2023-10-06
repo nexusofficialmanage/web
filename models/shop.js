9f1e2afe257c5967110a7e15f77bcb088e9a6346
jeevanalexen <86559072+castlercodes@users.noreply.github.com>
2023-10-06 09:55:01 +0530

    created user and shop schemas

--- models/shop.js
+++ models/shop.js
@@ -1,5 +1,43 @@
-import {Schema, model, models} from 'mongoose';
+import { Schema, model, models } from 'mongoose';
 
-const UserSchema = new Schema({
-    
-})
+const ShopSchema = new Schema({
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    shopName: {
+        type: String,
+        required: [true, 'Shop name is required'],
+    },
+    addressLine: {
+        type: String,
+        required: [true, 'Address line is required'],
+    },
+    description: String,
+    phoneNumber: {
+        type: String,
+        required: [true, 'Phone number is required'],
+    },
+    emailId: {
+        type: String,
+        required: [true, 'Email ID is required'],
+    },
+    shopType: String,
+    shopTiming: String,
+    pictures: [String],
+    openOnDays: [String],
+    productCategories: [String],
+    productListings: [String],
+    paymentMethods: [String],
+    shippingInfo: String,
+    returnPolicy: String,
+    refundPolicy: String,
+    shopLogo: String,
+    socialMediaLinks: [String],
+    termsAndConditions: String,
+    privacyPolicy: String,
+});
+
+const Shop = models.Shop || model('Shop', ShopSchema);
+
+export default Shop;
