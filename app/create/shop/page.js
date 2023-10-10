eb0d165dc87c8f4f842b90a73a402e5dcd7c3be4
jeevanalexen <86559072+castlercodes@users.noreply.github.com>
2023-10-10 11:53:23 +0530

    added styles tothe shop creation page

--- app/create/shop/page.js
+++ app/create/shop/page.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import './page.css';
 import { useSearchParams } from 'next/navigation';
 import { AiFillPlusCircle, AiOutlineMinusCircle } from 'react-icons/Ai';
-import {Tooltip, Button} from "@nextui-org/react";
+import { Tooltip } from "@nextui-org/react";
 
 function Page() {
   const searchParams = useSearchParams();
@@ -93,16 +93,16 @@ function Page() {
       <div className='bannercreateshop'></div>
       <div className='shopinformation'>
         <div className='imagealternator'>
-        {images.length === 0 ? (
+          {images.length === 0 ? (
             <img src="/assests/images/clement-fusil-Fpqx6GGXfXs-unsplash.jpg" alt="Default" className="default-image" />
-            ) : (
-              <img
+          ) : (
+            <img
               key={Number(currentIndex)}
               src={images[currentIndex]}
               alt={`Slide ${currentIndex + 1}`}
               // onClick={() => changeImage(index)}
-              />
-           )}
+            />
+          )}
           <input
             type="file"
             onChange={handleImageUpload}
@@ -110,127 +110,200 @@ function Page() {
           />
         </div>
         <div className='shopinformationinput'>
-        <div className='labelinput'>
-          <div>Shop Name: </div>
-          <input
-            placeholder="Enter your Shop's name"
-            className='inpt'
-            value={formData.shopName}
-            onChange={(e) => handleInputChange(e, 'shopName')}
-          />
-        </div>
-          <div className='labelinput'>
-            <div>ShopId: </div>
-            <Tooltip content='The name should be unique and without spaces and will be used to identify your store' className='tooltip'>
-              <input 
-                placeholder='Enter a unique shopname'
-                className='inpt'
-                value={formData.storeid}
-                onChange={(e) => handleInputChange(e, storeid)}
-              />
-            </Tooltip>
-          </div>
-          <div className='labelinput'>
-            <div>ShopTag Line: </div>
-            <Tooltip content='The tagline should be short and should be like a motto for your store' className='tooltip'>
-              <input
-                placeholder="Enter a ShopTagline"
-                className='inpt'
-                value={formData.shopTagline}
-                onChange={(e) => handleInputChange(e, 'shopTagline')}
-              />
-            </Tooltip>
-          </div>
-          <textarea
-            placeholder='Enter the description'
-            className='inpt textbox'
-            value={formData.description}
-            onChange={(e) => handleInputChange(e, 'description')}
-          />
-          <input
-            placeholder="Enter Address Line"
-            className='inpt'
-            value={formData.addressLine}
-            onChange={(e) => handleInputChange(e, 'addressLine1')}
-          />
-          <input
-            placeholder="Enter Pincode"
-            className='inpt'
-            value={formData.pincode}
-            onChange={(e) => handleInputChange(e, 'pincode')}
-          />
-          <input
-            placeholder="Enter City"
-            className='inpt'
-            value={formData.city}
-            onChange={(e) => handleInputChange(e, 'city')}
-          />
-          <input
-            placeholder="Enter State"
-            className='inpt'
-            value={formData.state}
-            onChange={(e) => handleInputChange(e, 'state')}
-          />
-          <input
-            placeholder="Enter Country"
-            className='inpt'
-            value={formData.country}
-            onChange={(e) => handleInputChange(e, 'country')}
-          />
-          <div className=''>
-            {formData.phoneNumbers.map((phoneNumber, index) => (
-              <div className='addoninput' key={index}>
-                <input
-                  className='inpt'
-                  type="text"
-                  value={phoneNumber}
-                  placeholder={`Phone Number ${index + 1}`}
-                  onChange={(e) => {
-                    const newPhoneNumbers = [...formData.phoneNumbers];
-                    newPhoneNumbers[index] = e.target.value;
-                    setFormData((prevData) => ({
-                      ...prevData,
-                      phoneNumbers: newPhoneNumbers,
-                    }));
-                  }}
-                />
-                {index > 0 && (
-                  <AiOutlineMinusCircle
-                    onClick={() => removePhoneNumberInput(index)}
-                    className='btncreate'
+          <table>
+            <tbody>
+              <tr>
+                <td><label htmlFor="shopName">Shop Name:</label></td>
+                <td>
+                  <input
+                    type="text"
+                    id="shopName"
+                    placeholder="Enter your Shop's name"
+                    className='inpt'
+                    value={formData.shopName}
+                    onChange={(e) => handleInputChange(e, 'shopName')}
                   />
-                )}
-              </div>
-            ))}
-            <AiFillPlusCircle onClick={addPhoneNumberInput} className='btncreate' />
-          </div>
-          <div className=''>
-            {formData.emailIds.map((emailId, index) => (
-              <div className='addoninput' key={index}>
-                <input
-                  className='inpt'
-                  type="text"
-                  value={emailId}
-                  placeholder={`Email Id ${index + 1}`}
-                  onChange={(e) => {
-                    const newEmailIds = [...formData.emailIds];
-                    newEmailIds[index] = e.target.value;
-                    setFormData((prevData) => ({
-                      ...prevData,
-                      emailIds: newEmailIds,
-                    }));
-                  }}
-                />
-                {index > 0 && (
-                  <AiOutlineMinusCircle
-                    onClick={() => removeEmailIdInput(index)}
-                    className='btncreate'
+                </td>
+              </tr>
+              <tr>
+                <td><label htmlFor="storeid">ShopId:</label></td>
+                <td>
+                  <Tooltip content='The name should be unique and without spaces and will be used to identify your store' className='tooltip'>
+                    <input 
+                      type="text"
+                      id="storeid"
+                      placeholder='Enter a unique shopname'
+                      className='inpt'
+                      value={formData.storeid}
+                      onChange={(e) => handleInputChange(e, 'storeid')}
+                    />
+                  </Tooltip>
+                </td>
+              </tr>
+              <tr>
+                <td><label htmlFor="shopTagline">Shop TagLine:</label></td>
+                <td>
+                  <Tooltip content='The tagline should be short and should be like a motto for your store' className='tooltip'>
+                    <input
+                      type="text"
+                      id="shopTagline"
+                      placeholder="Enter a ShopTagline"
+                      className='inpt'
+                      value={formData.shopTagline}
+                      onChange={(e) => handleInputChange(e, 'shopTagline')}
+                    />
+                  </Tooltip>
+                </td>
+              </tr>
+              <tr>
+                <td><label htmlFor="description">Description:</label></td>
+                <td>
+                  <Tooltip content='Write a description about your shop. You can make include as much details as you want. A description with keywords related to your store will improve search results for your store' className='tooltip'>
+                    <textarea
+                      id="description"
+                      placeholder='Enter the description'
+                      className='inpt textbox'
+                      value={formData.description}
+                      onChange={(e) => handleInputChange(e, 'description')}
+                    />
+                  </Tooltip>
+                </td>
+              </tr>
+              <tr>
+                <td><label htmlFor="addressLine">Address Line:</label></td>
+                <td>
+                  <Tooltip content='Enter your address line so that people can identify your shop' className='tooltip'>
+                    <input
+                      type="text"
+                      id="addressLine"
+                      placeholder="Enter Address Line"
+                      className='inpt'
+                      value={formData.addressLine}
+                      onChange={(e) => handleInputChange(e, 'addressLine')}
+                    />
+                  </Tooltip>
+                </td>
+              </tr>
+              <tr>
+                <td><label htmlFor="pincode">PinCode:</label></td>
+                <td>
+                  <input
+                    type="text"
+                    id="pincode"
+                    placeholder="Enter Pincode"
+                    className='inpt'
+                    value={formData.pincode}
+                    onChange={(e) => handleInputChange(e, 'pincode')}
                   />
-                )}
-              </div>
-            ))}
-            <AiFillPlusCircle onClick={addEmailIdInput} className='btncreate' />
-          </div>
+                </td>
+              </tr>
+              <tr>
+                <td><label htmlFor="city">City:</label></td>
+                <td>
+                  <input
+                    type="text"
+                    id="city"
+                    placeholder="Enter City"
+                    className='inpt'
+                    value={formData.city}
+                    onChange={(e) => handleInputChange(e, 'city')}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td><label htmlFor="state">State:</label></td>
+                <td>
+                  <input
+                    type="text"
+                    id="state"
+                    placeholder="Enter State"
+                    className='inpt'
+                    value={formData.state}
+                    onChange={(e) => handleInputChange(e, 'state')}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td><label htmlFor="country">Country:</label></td>
+                <td>
+                  <input
+                    type="text"
+                    id="country"
+                    placeholder="Enter Country"
+                    className='inpt'
+                    value={formData.country}
+                    onChange={(e) => handleInputChange(e, 'country')}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td><label htmlFor="phoneNumbers">Phone Number:</label></td>
+                <td>
+                  <div className=''>
+                    {formData.phoneNumbers.map((phoneNumber, index) => (
+                      <div className='addoninput' key={index}>
+                        <input
+                          className='inpt'
+                          type="text"
+                          id={`phoneNumbers_${index}`}
+                          value={phoneNumber}
+                          placeholder={`Phone Number ${index + 1}`}
+                          onChange={(e) => {
+                            const newPhoneNumbers = [...formData.phoneNumbers];
+                            newPhoneNumbers[index] = e.target.value;
+                            setFormData((prevData) => ({
+                              ...prevData,
+                              phoneNumbers: newPhoneNumbers,
+                            }));
+                          }}
+                        />
+                        {index > 0 && (
+                          <AiOutlineMinusCircle
+                            onClick={() => removePhoneNumberInput(index)}
+                            className='btncreate'
+                          />
+                        )}
+                      </div>
+                    ))}
+                    <AiFillPlusCircle onClick={addPhoneNumberInput} className='btncreate' />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td><label htmlFor="emailIds">Email Id:</label></td>
+                <td>
+                  <div className=''>
+                    {formData.emailIds.map((emailId, index) => (
+                      <div className='addoninput' key={index}>
+                        <input
+                          className='inpt'
+                          type="text"
+                          id={`emailIds_${index}`}
+                          value={emailId}
+                          placeholder={`Email Id ${index + 1}`}
+                          onChange={(e) => {
+                            const newEmailIds = [...formData.emailIds];
+                            newEmailIds[index] = e.target.value;
+                            setFormData((prevData) => ({
+                              ...prevData,
+                              emailIds: newEmailIds,
+                            }));
+                          }}
+                        />
+                        {index > 0 && (
+                          <AiOutlineMinusCircle
+                            onClick={() => removeEmailIdInput(index)}
+                            className='btncreate'
+                          />
+                        )}
+                      </div>
+                    ))}
+                    <AiFillPlusCircle onClick={addEmailIdInput} className='btncreate' />
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
