681f7b355f87092ebe96518fed305420040e4c39
jeevanalexen <86559072+castlercodes@users.noreply.github.com>
2023-10-15 14:19:14 +0530

    some new changes

--- app/create/shop/page.js
+++ app/create/shop/page.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import './page.css';
 import { useSearchParams } from 'next/navigation';
-import { AiFillPlusCircle, AiOutlineMinusCircle } from 'react-icons/Ai';
+import { AiFillPlusCircle, AiOutlineMinusCircle } from 'react-icons/ai';
 import { Tooltip } from "@nextui-org/react";
 import { useUser } from '@auth0/nextjs-auth0/client';
 
