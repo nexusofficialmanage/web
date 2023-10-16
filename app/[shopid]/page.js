0876cf3ec049dd4611ee3aa7046a14bde10e1244
jeevanalexen <86559072+castlercodes@users.noreply.github.com>
2023-10-16 19:05:29 +0530

    changes in home page

--- app/[shopid]/page.js
+++ app/[shopid]/page.js
@@ -15,15 +15,17 @@ function Page() {
         <div className="bannerinfo">
           <div className="bannershopname">Nexus</div>
           <div className="bannershoptagline">Lorem Ipsum Lorem Ipsum Lorem Ipsum</div>
-          <div className="shopnowbutton">Shop Now</div>
+          <div className="shopnowbutton"><a href='#shopnow'>Shop Now</a></div>
         </div>
         <div className="bannerimage">
-          <img src="/assests/images/shopimage.jpg" className="zoomIn" alt="Castle" />
+          <img src="/assests/images/castle.jpg" className="zoomIn" alt="Castle" />
         </div>
         <div className='viewcards'>
-          <div className='viewcard'>
-            <div>Description</div>
-            <div className='viewcarddes'>Learn More about us</div>
+        <div className='viewcard'>
+            <a href='#description'>
+              <div>Description</div>
+              <div className='viewcarddes'>Learn More about us</div>
+            </a>
           </div>
           <div className='viewcard'>
             <div>Review</div>
@@ -31,7 +33,18 @@ function Page() {
           </div>
         </div>
       </div>
-      <div className='products'>
+      <div className='shopdetails'>
+        <div className='descriptiondetails' id='description'>
+          <div className='descriptiondetailswriting'>
+            <div className='descriptiondetailstitle'>Here is a little bit about <span style={{color:'red'}}>Ourselves</span></div>
+            <div className='descriptiondetailstext'>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tempus massa nec vulputate condimentum. Nam ornare ullamcorper placerat. Etiam semper eros et tincidunt fringilla. Nunc luctus libero et justo aliquam tempor. Sed sodales vulputate dolor, in convallis ex fermentum eleifend. Nunc mauris est, mattis ac risus auctor, sollicitudin molestie ante. Nam suscipit venenatis tellus et rhoncus. Nulla eu suscipit leo. Cras eu fermentum quam, placerat vehicula ipsum. Nam elementum augue eu consectetur tempor. Integer consectetur sapien vel lacinia semper. Quisque at ex eget erat pretium scelerisque eu nec leo.
+            </div>
+          </div>
+          <div className='descriptionimage'><img src='/assests/images/descriptionvector1.png'/></div>
+        </div>
+      </div>
+      <div className='products' id='shopnow'>
         {
           products.map((product) => {
             return <ProductCard product={product}/>
